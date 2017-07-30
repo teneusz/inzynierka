5006432131ff9788c35260305ee37d6bf4e1eaaa
Teneusz <teneusz@wp.pl>
2017-07-30 11:32:14 +0200

    init commit

---
+++ dream-service/src/main/java/pl/teneusz/dzienniksnow/dreamservice/DreamServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.teneusz.dzienniksnow.dreamservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamServiceApplication.class, args);
+	}
+}
