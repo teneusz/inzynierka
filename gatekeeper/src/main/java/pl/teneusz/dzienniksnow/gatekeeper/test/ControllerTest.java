cbdda714b13a9659585f8770daacc733027740b0
Teneusz <teneusz@wp.pl>
2017-08-15 10:40:12 +0200

    gatekeeper update

--- gatekeeper/src/main/java/pl/teneusz/dzienniksnow/gatekeeper/test/ControllerTest.java
+++ gatekeeper/src/main/java/pl/teneusz/dzienniksnow/gatekeeper/test/ControllerTest.java
@@ -6,20 +6,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+
 @Controller(value = "/")
 public class ControllerTest {
-//    @Autowired
-//    RestTemplate restTemplate;
-
+    @Autowired
+    RestTemplate restTemplate;
+    @Autowired
+    LoadBalancerClient loadBalancer;
 
     @RequestMapping(method = RequestMethod.GET,path = "/test")
     public User[] test(){
-        return new RestTemplate().getForObject("http://user-service/user",User[].class);
+        ServiceInstance instance = loadBalancer.choose("user-service");
+        URI uri = instance.getUri();
+        return restTemplate.getForObject(uri.toString()+"/user",User[].class);
     }
 
     public User[] testDefault()
